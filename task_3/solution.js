12710a4bd5ef300dc0eef4a86722bcdd95d75303
Irina <issphere5@gmail.com>
2022-04-12 10:07:28 +0300

    work3.3

--- task_3/solution.js
+++ task_3/solution.js
@@ -3,7 +3,8 @@ function renderCartItem(item) {
 
     // Задание №3.1. Формирование строки корзины
 
-    // product - объект вида {id: id, title: title, count: count, price: price}
+let product = {id: 1, title: "рулет", count: 15, price: '100.00'}    
+// product - объект вида {id: id, title: title, count: count, price: price}
     // например, {id: 1, title: 'Пицца', count: 5, price: '500.00'}, где
     // id - идентификатор
     // title - наименование

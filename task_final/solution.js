4294957c455864c05f2c9a74f5af8910f1fcbaf1
Irina <issphere5@gmail.com>
2022-04-26 01:11:32 +0300

    work5.6

--- task_final/solution.js
+++ task_final/solution.js
@@ -36,7 +36,7 @@
 function sendRequest(name, phone, address, goods, sum) {
     let data = {client: name + phone,
     order: {address: "", sum: 1},
-    goods: [{title, count}]
+    goods: [{product.title, product.count}]
 };
 
     let countOfGoods = goods.length;
